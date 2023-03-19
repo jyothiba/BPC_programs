9563f5054fb827670e133826a68cd4d997ab7d22
jyothiba <128213866+jyothiba@users.noreply.github.com>
2023-03-19 19:25:49 +0530

    Add files via upload

---
+++ bpc/natural.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int num, i;
+    printf("enter the number:");
+    scanf("%d", &num);
+    for(i=1; i<=num;i++)
+
+    switch(i<=num)
+    {
+       case 1 : printf("%d \n",i);
+        break;
+        default : exit(0);
+    }
+   
+}
