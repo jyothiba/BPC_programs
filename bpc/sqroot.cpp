2b69f8e5d8c12a1c6732e4e29f5caa798a4d1138
jyothiba <128213866+jyothiba@users.noreply.github.com>
2023-05-28 22:03:04 +0530

    Add files via upload

---
+++ bpc/sqroot.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	float number, sqrtResult;
+	
+	cout << "\n Enter any number =  ";
+	cin >> number;
+	
+	sqrtResult = sqrt(number);
+	
+	cout << "\n  Sqauer root of a " << number << " = " << sqrtResult;
+	
+ 	return 0;
+}
