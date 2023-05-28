fa6577d61f72d96b3a18909b2b5f48f17a19820a
jyothiba <128213866+jyothiba@users.noreply.github.com>
2023-05-28 15:44:26 +0530

    Add files via upload

---
+++ bpc/float.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    float num1,num2,product;
+    printf("enter two nos");
+    scanf("%f%f",&num1,&num2);
+    product=num1*num2;
+    printf("product=%f",product);
+
+}
